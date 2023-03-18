9d4b7c3f0cdb5616a0387805b8a7f0f7d67ce366
voidshine <voidshine@outlook.com>
2023-03-18 12:53:24 -0700

    Draft bayan layout

--- schism/midi-core.c
+++ schism/midi-core.c
@@ -122,6 +122,42 @@ int midi_c5note = 60;
 
 #define CFG_GET_MI(v,d) midi_ ## v = cfg_get_number(cfg, "MIDI", #v, d)
 
+#if VOIDSHINE
+// Axis 49 mapped to Chromatic Button Accordion B-griff (Bayan)
+uint8_t midi_input_note_map[128] = {
+ 48, 49, 50, 51, 52, 53, 54,
+   51, 52, 53, 54, 55, 56, 57,
+ 53, 54, 55, 56, 57, 58, 59,
+   56, 57, 58, 59, 60, 61, 62,
+ 58, 59, 60, 61, 62, 63, 64,
+   61, 62, 63, 64, 65, 66, 67,
+ 63, 64, 65, 66, 67, 68, 69,
+   66, 67, 68, 69, 70, 71, 72,
+     69, 70, 71, 72, 73, 74, 75,
+   71, 72, 73, 74, 75, 76, 77,
+     74, 75, 76, 77, 78, 79, 80,
+   76, 77, 78, 79, 80, 81, 82,
+     79, 80, 81, 82, 83, 84, 85,
+   81, 82, 83, 84, 85, 86, 87,
+// 98 keys above, leaving 30 unampped:
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+#else
+// Identity
+uint8_t midi_input_note_map[128] = {
+  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+ 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+ 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+ 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+ 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+ 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+ 96, 97, 98, 99,100,101,102,103,104,105,106,107,108,109,110,111,
+112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,
+};
+#endif
+
 static void _cfg_load_midi_part_locked(struct midi_port *q)
 {
 	struct cfg_section *c;
@@ -965,7 +1001,11 @@ int midi_engine_handle_event(void *ev)
 			kk.state = KEY_RELEASE;
 		}
 		kk.midi_channel = st[1]+1;
-		kk.midi_note = (st[2]+1 + midi_c5note) - 60;
+		if (st[2] < 128) {
+			kk.midi_note = (midi_input_note_map[st[2]]+1 + midi_c5note) - 60;
+		} else {
+			kk.midi_note = (st[2]+1 + midi_c5note) - 60;
+		}
 		if (midi_flags & MIDI_RECORD_VELOCITY)
 			kk.midi_volume = st[3];
 		else
